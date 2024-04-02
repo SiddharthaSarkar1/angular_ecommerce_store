7d93c0175bde8375cad6ae5e002ad9fe4d1478a7
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-04-03 02:54:23 +0530

    home page in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,9 +23,12 @@ import { CommonModule } from '@angular/common';
 
 import { ToastrModule } from 'ngx-toastr';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
+import { FiltersComponent } from './pages/home/components/filters/filters.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, ProductsHeaderComponent, FiltersComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html',
+  styleUrls: ['./filters.component.css']
+})
+export class FiltersComponent {
+  categories = ['shoes', 'smartphones', 'laptops', 'sports'];
+  constructor() {}
+
+  ngOnInit() {}
+}
